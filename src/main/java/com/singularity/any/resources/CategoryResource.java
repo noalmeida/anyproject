ba672ad1afabc2d0bf3595b48b48555909439a96
Noam Almeida <noamwt@gmail.com>
2021-01-30 00:52:16 -0300

    Testing first class domain

--- src/main/java/com/singularity/any/resources/CategoryResource.java
+++ src/main/java/com/singularity/any/resources/CategoryResource.java
@@ -1,8 +1,13 @@
 package com.singularity.any.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.singularity.any.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
@@ -10,7 +15,17 @@ public class CategoryResource {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Category> listar() {
+		
+		
+		Category cat1 = new Category(1, "tecnology");
+		Category cat2 = new Category(2, "office");
+		
+		List <Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+		
 	}
 }
