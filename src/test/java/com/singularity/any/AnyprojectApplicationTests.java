821bd3ba12f2b3dcab0da26dbd52dc91ba279b48
Noam Almeida <noamwt@gmail.com>
2021-01-29 23:56:51 -0300

    create project

---
+++ src/test/java/com/singularity/any/AnyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singularity.any;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
